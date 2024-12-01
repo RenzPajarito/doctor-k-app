547b30f73d88331d7b82bdac835099d71bfb361d
Renz Pajarito <renzpajarito08@gmail.com>
2024-12-01 17:45:49 +0800

    minor update

--- screens/OrderPage.tsx
+++ screens/OrderPage.tsx
@@ -3,14 +3,14 @@ import { WebView } from "react-native-webview";
 import { useMemo } from "react";
 
 export default function OrderPage() {
-  const webViewSource = useMemo(() => ({ uri: "https://doctor-k.vercel.app?table=0" }), []);
+  const webViewSource = useMemo(
+    () => ({ uri: "https://doctor-k.vercel.app?table=0" }),
+    []
+  );
 
   return (
     <View style={styles.container}>
-      <WebView 
-        source={webViewSource}
-        style={styles.webView}
-      />
+      <WebView source={webViewSource} style={styles.webView} />
     </View>
   );
 }
